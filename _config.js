cafb18c39f0c43a1c781e9fa7317650e23479787
Ayub Nyaga <nyagaayub9@gmail.com>
2024-07-10 11:25:59 +0300

    merging test branch

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/',
-    development: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/',
-    test: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/',
+    production: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
