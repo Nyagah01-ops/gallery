5d44bc1ffe3fc30618f8be52b67cc762b8de0236
Ayub Nyaga <nyagaayub9@gmail.com>
2024-07-10 12:06:43 +0300

    modified package .json

--- _config.js
+++ _config.js
@@ -5,5 +5,6 @@ config.mongoURI = {
     production: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     development: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     test: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  
 }
 module.exports = config;
