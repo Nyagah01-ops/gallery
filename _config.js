c79a7d6e35d46aec0d9005e5ac02b54d778d6a2d
Ayub Nyaga <nyagaayub9@gmail.com>
2024-07-10 11:06:34 +0300

    saving test code

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/',
-    development: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/',
-    test: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/',
+    production: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/darkroom?retryWrites=true&w=majority',
+    test: 'mongodb+srv://nyagaayub9:nyaga123@cluster0.go09a5e.mongodb.net/darkroom?retryWrites=true&w=majority', 
+    
 }
 module.exports = config;
